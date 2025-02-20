e6c9bdf83d9e23e78d04a400ca973975833fba5b
Jorge Zavala <kokeaft@gmail.com>
2025-02-20 00:37:44 -0300

    first commit

---
+++ one-challenge-javascript.js
@@ -0,0 +1,24 @@
+let Saludo = "Esta es mi primera variable String";
+
+
+let operadoresAritmeticos = [
+    "- Resta", 
+    "* Multiplicación", 
+    "+ Suma", 
+    "/ División", 
+    "% Modulo", 
+    "** Exponenciación", 
+    "++ Incremento", 
+    "-- Decremento"] ;
+
+
+let operadoresDeAsignacion = [
+    "= Igual",
+    "+= Asignación con suma, (X += Y  es equivalente x = x + y)",
+    "-= Asignación con resta",
+    "*= Asignación con multiplicación",
+    "/= Asignación con división",
+    "%= Asignación con modulo",
+    "**= Asignación con exponenciación",
+    
+]
---
+++ readme.md
@@ -0,0 +1,3 @@
+Esta es una guia de javascript cual tendra la documentación de comó se ejecuta el lenguaje en sí.
+
+ire subiendo ejercicios y actualizando este proyecto.
